9e9affa3ae16a6a31817cdd1962c6775ee2fded0
GodfriedN <145958984+GodfriedN@users.noreply.github.com>
2024-10-14 19:35:55 +0200

    Update kafka_setup.sh

--- kafka/kafka_setup.sh
+++ kafka/kafka_setup.sh
@@ -5,3 +5,4 @@ docker network create kafka-network
 docker run -d --network=kafka-network --name zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 confluentinc/cp-zookeeper:latest
 
 docker run -d --network=kafka-network --name kafka -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=PLAINTEXT:PLAINTEXT -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 confluentinc/cp-kafka:latest
+//end
